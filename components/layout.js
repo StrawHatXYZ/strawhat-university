9d05181312fcf3833ab7c36b41f27b4bf1608d53
Sandeep Krishna <sandeep@MacBook-Pro-2.local>
2022-12-03 13:51:32 +0530

    Fix: Bugs in Sidebar

--- components/layout.js
+++ components/layout.js
@@ -1,82 +1,116 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import Logo from './logo';
-import {MdDashboard, MdFace, MdSettings, MdBook, MdAccountCircle, MdOutlineAccountCircle, MdAutoGraph, MdSupport, MdContactMail, MdInfo, MdLogout}  from 'react-icons/md';
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Logo from "./logo";
+import {
+  MdDashboard,
+  MdFace,
+  MdSettings,
+  MdBook,
+  MdAccountCircle,
+  MdOutlineAccountCircle,
+  MdAutoGraph,
+  MdSupport,
+  MdContactMail,
+  MdInfo,
+  MdLogout,
+} from "react-icons/md";
 
 export default function Layout({ children }) {
   const router = useRouter();
 
   const menuItems = [
     {
-      href: '/',
-      title: 'Dashboard',
-      icon : <MdDashboard />
+      href: "/",
+      title: "Dashboard",
+      icon: <MdDashboard />,
     },
     {
-        href: '/activity',
-        title: 'Activity',
-        icon : <MdAutoGraph />
-      },
+      href: "/activity",
+      title: "Activity",
+      icon: <MdAutoGraph />,
+    },
     {
-        href: '/library',
-        title: 'Library',
-        icon : <MdBook />
-      },
-      {
-        href: '/support',
-        title: 'Support',
-        icon : <MdSupport />
-      },
-      {
-        href: '/profile',
-        title: 'Profile',
-        icon : <MdOutlineAccountCircle />
-      },
-      {
-        href: '/about',
-        title: 'About',
-        icon : <MdInfo />
-      },
-      {
-        href: '/settings',
-        title: 'Settings',
-        icon : <MdSettings />
-      },
+      href: "/library",
+      title: "Library",
+      icon: <MdBook />,
+    },
+    {
+      href: "/support",
+      title: "Support",
+      icon: <MdSupport />,
+    },
+    {
+      href: "/profile",
+      title: "Profile",
+      icon: <MdOutlineAccountCircle />,
+    },
+    {
+      href: "/about",
+      title: "About",
+      icon: <MdInfo />,
+    },
+    {
+      href: "/settings",
+      title: "Settings",
+      icon: <MdSettings />,
+    },
   ];
 
   return (
-    <div className='min-h-screen flex flex-col'>
-      <div className='flex flex-col md:flex-row flex-1'>
-        <aside className='bg-white w-full md:w-1/5'>
-          <nav >
-            <ul>
-            <Logo />
-              {menuItems.map(({ href, title,icon }) => (
-                <li className='my-3' key={title}>
-                  <Link href={href} legacyBehavior>
-                    <a
-                      className={`flex px-6 py-3 items-center rounded  hover:bg-primary-100 cursor-pointer ${
-                        router.asPath === href ? 'text-black font-semibold border-r-4 border-primary-500': 'text-secondary-600'
-                      }`}
-                    >
-                      <i className={`px-4 text-2xl ${router.asPath === href && 'text-primary-500  font-semibold'}`}>{icon}</i> {title}
-                    </a>
+    <div class="flex">
+      <aside class="h-screen sticky top-0 bg-white w-full md:w-72">
+        <nav className="h-full">
+          <ul className="h-full">
+            <div className="flex flex-col justify-between h-full">
+              <div>
+                <Logo />
+                {menuItems.map(({ href, title, icon }) => (
+                  <li className="my-3" key={title}>
+                    <Link href={href} legacyBehavior>
+                      <a
+                        className={`flex px-6 py-3 items-center rounded hover:text-secondary-700 hover:bg-primary-100 cursor-pointer ${
+                          router.asPath === href
+                            ? "text-black font-semibold border-r-4 border-primary-500"
+                            : "text-secondary-600"
+                        }`}
+                      >
+                        <i
+                          className={`px-4 text-2xl ${
+                            router.asPath === href &&
+                            "text-primary-500  font-semibold"
+                          }`}
+                        >
+                          {icon}
+                        </i>{" "}
+                        {title}
+                      </a>
+                    </Link>
+                  </li>
+                ))}
+              </div>
+              <div>
+                <li className="my-3 border-t border-secondary-500" key="logout">
+                  <Link
+                    href="/logout"
+                    className={`flex px-6 py-4 items-center rounded  hover:bg-primary-100 hover:text-secondary-700 cursor-pointer ${
+                      router.asPath === "/logout"
+                        ? "text-black font-semibold border-r-4 border-primary-500"
+                        : "text-secondary-600"
+                    }`}
+                  >
+                    <i className="px-4 text-2xl">
+                      <MdLogout />
+                    </i>{" "}
+                    Logout
                   </Link>
                 </li>
-              ))}
-            <div className='flex h-52 flex-col  justify-end'>
-                <li className=''>
-                    <Link href='/logout'  className="flex px-6 py-3 items-center rounded  hover:bg-primary-100 cursor-pointer text-secondary-600">
-                        <i className='px-4 text-2xl'><MdLogout /></i> Logout
-                    </Link>
-                </li>
+              </div>
             </div>
+          </ul>
+        </nav>
+      </aside>
 
-            </ul>
-          </nav>
-        </aside>
-        <main className='flex-1'>{children}</main>
-      </div>
+      <main className="flex-1 ">{children}</main>
     </div>
   );
 }
