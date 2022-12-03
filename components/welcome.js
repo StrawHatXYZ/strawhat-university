727787f1f613b45e4d1397b43e02c9c3243ed4cd
Sandeep Krishna <sandeep@strawhat.xyz>
2022-12-03 14:21:26 +0530

    Test

---
+++ components/welcome.js
@@ -0,0 +1,14 @@
+export default function Welcome() {
+  return (
+    <div className="flex justify-between mx-10 mt-20 ">
+      <p className="text-4xl mb-5 font-bold">Welcome Sandeep Krishna</p>
+      <div>
+        <div className="flex">
+          <div className="flex flex-col items-center">
+            <input type="text" value="0" className="text-center" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
